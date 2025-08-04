2a89627687227696b8d79465a1460cc7389990b3
sauravswarajj <sauravswaraj.dev@gmail.com>
2025-08-04 13:00:39 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//  alert(3 + 3) //we are using nodejs, not browser, so alert will not work
+
+console.log(3 
+    + 3) //code readability, should be high
+
+
+console.log("Saurav");
+
+
+let name = "Saurav"
+let age = 25
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+//bigint => 2 to power 53 and above
+//string => "Saurav", 'Saurav', `Saurav`
+// boolean => true, false
+// null => empty value
+// undefined => variable is not defined
+// symbol => unique identifier
+
+
+// object => collection of key-value pairs
+
+console.log(typeof undefined); // udefined
+console.log(typeof null); // object, this is a bug in JS, null is not an object
+
