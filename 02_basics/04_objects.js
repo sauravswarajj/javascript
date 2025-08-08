1b41e6f13501ab3587361623b9e8c62435b05eee
sauravswarajj <sauravswaraj.dev@gmail.com>
2025-08-08 14:40:16 +0530

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -57,12 +57,56 @@ const users = [
     },
 ]
  users[1].email
- console.log(tinderUser); // { id: '123abc', name: 'Saurav', isLoggedIn: false }
+//  console.log(tinderUser); // { id: '123abc', name: 'Saurav', isLoggedIn: false }
 
-console.log(Object.keys(tinderUser)); // ['id', 'name', 'isLoggedIn'], returns an array of keys in the object
-console.log(Object.values(tinderUser)); // ['123abc', 'Saurav', false], returns an array of values in the object
-console.log(Object.entries(tinderUser)); // [['id', '123abc'], ['name', 'Saurav'], ['isLoggedIn', false]], returns an array of key-value pairs in the object
+// console.log(Object.keys(tinderUser)); // ['id', 'name', 'isLoggedIn'], returns an array of keys in the object
+// console.log(Object.values(tinderUser)); // ['123abc', 'Saurav', false], returns an array of values in the object
+// console.log(Object.entries(tinderUser)); // [['id', '123abc'], ['name', 'Saurav'], ['isLoggedIn', false]], returns an array of key-value pairs in the object
 
-console.log(tinderUser.hasOwnProperty("id")); // true, checks if the object has a property 'id'
-console.log(tinderUser.hasOwnProperty("isLoggedIn")); // true, checks if the object has a property 'isLoggedIn'
-console.log(tinderUser.hasOwnProperty("isLogged")); // false, checks if the object has a property 'isLoggedIn' (note the typo)
+// console.log(tinderUser.hasOwnProperty("id")); // true, checks if the object has a property 'id'
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); // true, checks if the object has a property 'isLoggedIn'
+// console.log(tinderUser.hasOwnProperty("isLogged")); // false, checks if the object has a property 'isLoggedIn' (note the typo)
+
+
+// Destructuring an object
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "Hitesh"
+}
+
+// course.courseInstructor
+
+//most preferable way to destructure an object
+const {courseInstructor} = course; // Extracting courseInstructor from course object
+
+// now we destructure it like
+const { courseInstructor: instructor } = course; // Renaming courseInstructor to instructor this is called destructuring
+console.log(courseInstructor); // Hitesh
+console.log(instructor); // Hitesh , value will be same as courseInstructor
+
+
+
+// in React we saw this kind of destructuring then understand its getting logic from javascript
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "Hitesh");
+
+// api se data JSON format me aata h, aur wo aisa dikhta hai
+
+// {
+//     "name": "hitesh" ,
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+//some  time we also see like this
+
+// [ 
+// {}, 
+// {}, 
+// {}
+// ]
